8007e2f1b7a1a62c16bba1a919b3300bf741e998
chaker <luisgas012@gmail.com>
2024-09-13 20:03:30 -0500

    Update

--- enviarCorreo.sh
+++ enviarCorreo.sh
@@ -1,4 +1,4 @@
 {
-  echo "Subject: Mi nota Quiz Linux - Luis Gabriel Romero Castro"
-  echo "Mi nombre es Luis Gabriel Romero Castro y mi nota es 5.0"
-} | msmtp -a gmail luisgabrielromerocastro789@gmail.com
+  echo "Subject: Mi nota Quiz Linux - Luis"
+  echo "Mi nombre es Luis"
+} | msmtp -a gmail prueba@gmail.com
