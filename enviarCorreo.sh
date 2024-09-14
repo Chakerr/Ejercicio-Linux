b93320f220b64cda39b044308e3848a6517a12ab
chaker <luisgas012@gmail.com>
2024-09-13 19:59:02 -0500

    Update

---
+++ enviarCorreo.sh
@@ -0,0 +1,4 @@
+{
+  echo "Subject: Mi nota Quiz Linux - Luis Gabriel Romero Castro"
+  echo "Mi nombre es Luis Gabriel Romero Castro y mi nota es 5.0"
+} | msmtp -a gmail luisgabrielromerocastro789@gmail.com
