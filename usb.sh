971f66f26457fb4068ab2731162b21a8d1824a3c
chaker <luisgas012@gmail.com>
2024-09-13 20:08:21 -0500

    Update

---
+++ usb.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+usb_buscar="058f:6387"
+usb_mount_point="/media/chaker/LUIS/inicio.txt"
+numero_buscar="024"
+
+# Función para buscar archivo y número en el archivo
+buscar_archivo() {
+    if [ -f "$usb_mount_point" ]; then
+        echo "Archivo encontrado en el dispositivo USB."
+
+        if grep -q "$numero_buscar" "$usb_mount_point"; then
+            echo "El número $numero_buscar fue encontrado en el archivo."
+            return 0
+        else
+            echo "El número $numero_buscar NO fue encontrado en el archivo."
+            return 1
+        fi
+    else
+        echo "Archivo no encontrado en el dispositivo USB."
+        return 1
+    fi
+}
+
+# Función para obtener el puerto USB
+obtener_puerto_usb() {
+    
+    bus_device=$(lsusb | grep "$usb_buscar" | awk '{print $2"/"$4}' | sed 's/://')
+
+    if [ -n "$bus_device" ]; then
+        echo "El puerto es: /dev/bus/usb/$bus_device"
+        udevadm info --name=/dev/bus/usb/$bus_device | grep "DEVPATH"
+    else
+        echo "No se pudo encontrar el puerto del dispositivo USB."
+    fi
+}
+
+usb_conectado=false
+
+# Bucle principal para monitorear la conexión USB
+while true; do
+    if lsusb | grep -q "$usb_buscar"; then
+        if ! $usb_conectado; then
+            echo "La USB ha sido conectada"
+            usb_conectado=true
+
+            # Mostrar puerto USB
+            obtener_puerto_usb
+
+            sleep 5  
+        fi
+
+        sleep 2
+        
+        buscar_archivo
+        if [ $? -eq 0 ]; then
+            echo "Proceso terminado: archivo y número encontrados."
+            /home/chaker/Documentos/Ejercicio-Linux-main/especificaciones_pc.sh
+            /home/chaker/Documentos/Ejercicio-Linux-main/pagina.sh
+            /home/chaker/Documentos/Ejercicio-Linux-main/enlaceSimbolico.sh
+	        /home/chaker/Documentos/Ejercicio-Linux-main/enviarApache.sh
+            /home/chaker/Documentos/Ejercicio-Linux-main/encenderLedMayus.sh
+	        /home/chaker/Documentos/Ejercicio-Linux-main/enviarCorreo.sh
+            break
+        else
+            echo "Finalizando bucle: archivo o número no encontrados."
+            break
+        fi
+    else
+        if $usb_conectado; then
+            echo "La USB no se encuentra conectada"
+            usb_conectado=false
+        fi
+    fi
+    sleep 5 
+done
+
+
